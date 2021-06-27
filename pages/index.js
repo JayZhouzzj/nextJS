e0c7867c3d4567e547a98df65f02f232d8694532
Zhengjie Zhou (Jay) <zzj@dal.ca>
2021-06-27 02:16:56 -0300

    jjjj

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1>LOL</h1>
+        <h2>JJJJ</h2>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
